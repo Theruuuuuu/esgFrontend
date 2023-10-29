fbe1deec9769556c567da14e74ef45940cf7a955
Spranos <a12790342@gmail.com>
2023-10-29 21:04:33 +0800

    主頁排版修改及登入頁面美化 動畫待更新

--- src/app/content/content.component.ts
+++ src/app/content/content.component.ts
@@ -11,16 +11,30 @@ export class ContentComponent implements OnInit {
   constructor(private http:HttpClient) {}
   ngOnInit(): void {
   }
-  Dokeyup(data:string){
-    if(data!=''){
-      this.http.get('https://esgprojapi.azurewebsites.net/api/Content/search?search='+data).subscribe(u=>{
+  Dokeyup(data: string) {
+    if (data !== '') {
+      if (data.length > 5) {
+        // 如果输入字符数超过五个，截断字符串为前五个字符
+        data = data.slice(0, 5);
+      }
+  
+      this.http.get('https://esgprojapi.azurewebsites.net/api/Content/search?search=' + data).subscribe(u => {
         console.log(u);
-        this.getJsonData=u;
+  
+        // 检查响应数据是否是数组
+        if (Array.isArray(u)) {
+          // 截取返回结果的前五项
+          this.getJsonData = u.slice(0, 5);
+        } else {
+          // 响应数据不是数组，可能需要其他处理
+          // 在这里，您可以根据响应数据的类型执行适当的操作
+          console.error('响应数据不是数组');
+        }
       });
-    }else{
+    } else {
       this.getJsonData = null;
     }
-    
   }
+  
   
 }
